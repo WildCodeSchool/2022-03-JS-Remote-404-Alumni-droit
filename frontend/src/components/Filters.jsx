fce7b963cc079f9b83275b485df642faba3fe0aa
ismamamama1 <ismael.agb@hotmail.fr>
2022-06-03 11:45:04 +0200

    Created new dropdown with fake data list

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
+import { TextField, Autocomplete } from "@mui/material";
+
+const Diplomes = [
+  { label: "Diplôme du Collège de Droit" },
+  { label: "Certificat de l'École de Droit" },
+  { label: "Diplôme de l'École de Droit" },
+];
+
 function Filters() {
   return (
-    <div className="flex justify-center m-3 pt-0">
-      <input
-        type="text"
-        placeholder="Rechercher votre nom"
-        className="w-96 px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring ring-red-800"
+    <div className="flex justify-center m-3">
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={Diplomes}
+        sx={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Diplômes" color="primary" />
+        )}
       />
     </div>
   );
