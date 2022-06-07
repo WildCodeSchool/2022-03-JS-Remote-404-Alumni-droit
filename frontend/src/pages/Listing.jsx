e132f7b9b2b837350d92a6686260e93da216ac0d
limchristian81@gmail.com <limchristian81@gmail.com>
2022-06-07 15:03:19 +0200

    UserCard OK without preview

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+// import { useState, useEffect } from 'react';
+
+import UserCard from "@components/UserCard";
+
 function Listing() {
-  return <div>Listing</div>;
+  // const [alumnis, setAlumnis] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get("")
+  //     .then(res => setAlumnis(res.data))
+  //     .catch(err => console.error(err))
+  // }, [])
+
+  return (
+    <div className="flex flex-wrap justify-center p-2">
+      {/* MAIN */}
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+      <UserCard />
+    </div>
+  );
 }
 
 export default Listing;
