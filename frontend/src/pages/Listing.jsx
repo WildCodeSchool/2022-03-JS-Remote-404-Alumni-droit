48c7f064e586abee155a732221b1f9277be8ecaa
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-06-14 16:31:31 +0200

    FIXED axios (props in a object)

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -12,7 +12,6 @@ function Listing() {
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_BACKEND_URL}/annuaire`)
-      // .get("http://localhost:5000/annuaire")
       .then((res) => setRows(res.data))
       .catch((err) => console.error(err));
   }, []);
