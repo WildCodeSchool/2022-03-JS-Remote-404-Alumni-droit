5d821309d9bb04ea60f05c400ec74a1219c81f7b
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-06-20 15:33:49 +0200

    UPDATED database to increase compatibility on deploy + CORRECTION on column 'profession_id(1)'

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -56,7 +56,7 @@ function Listing() {
               lastname={row.lastname}
               firstname={row.firstname}
               photo={row.photo}
-              job={row.profession_id1}
+              job={row.profession_id}
             />
           ))}
       </div>
