5f2bb3cae7a60638d39aeec51dd750da1a902938
ismamamama1 <ismael.agb@hotmail.fr>
2022-06-23 14:51:34 +0200

    Created get routes for Filters

---
+++ backend/src/models/FilterDiplomeManager.js
@@ -0,0 +1,7 @@
+const AbstractManager = require("./AbstractManager");
+
+class FilterDiplomeManager extends AbstractManager {
+  static table = "diplome";
+}
+
+module.exports = FilterDiplomeManager;
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
-const { ProfileController } = require("./controllers");
+const {
+  ProfileController,
+  FilterDiplomeController,
+  FilterProfessionController,
+  FilterPromotionController,
+} = require("./controllers");
 
 const router = express.Router();
 
 router.get("/annuaire", ProfileController.browse);
 router.get("/annuaire/:id", ProfileController.read);
+
+router.get("/diplome", FilterDiplomeController.browse);
+router.get("/profession", FilterProfessionController.browse);
+router.get("/promotion", FilterPromotionController.browse);
 
 // router.put("/items/:id", ItemController.edit);
 // router.post("/items", ItemController.add);
