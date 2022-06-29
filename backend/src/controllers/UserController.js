9c1979527406d0278a78b4d9b698c8e7f157de14
Julien ichard WCS <julien.y.richard@gmail.com>
2022-06-29 16:26:32 +0200

    cleaning the project before implementing auth in frontend

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,6 +1,5 @@
-const models = require("../models");
-
 const { v4: uuidv4 } = require("uuid");
+const models = require("../models");
 const { passwordHash, passwordVerify } = require("../services/password");
 const { jwtSign } = require("../services/jwt");
 
