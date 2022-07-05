a7aae8f58cdbcfaf34004d25c16d4eb62d365d77
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2022-07-05 22:41:00 +0200

    ADDED links inside RGPD text

--- frontend/src/pages/RGPD.jsx
+++ frontend/src/pages/RGPD.jsx
@@ -17,18 +17,20 @@ function RGPD() {
         tiers. Les utilisateurs peuvent à tout moment supprimer leur profil et
         ainsi effacer toutes leurs données. Ils peuvent aussi contacter le
         secrétaire général de l’Association ou son adjoint à l’adresse
-        cdd.edd.paris2@gmail.com pour faire supprimer leur profil.
+        <a href="mailto:cdd.edd.paris2@gmail.com" target="_blank">
+          cdd.edd.paris2@gmail.com
+        </a>{" "}
+        pour faire supprimer leur profil.
       </p>
 
       <p className="w-[90%] text-gray-400 text-xs mx-auto my-4">
-        L’Association du Collège et de l’Ecole de droit
-        (cdd.edd.paris2@gmail.com) se réserve la possibilité d’utiliser les
-        informations disponibles sur le site de l’annuaire, aux fins de tenir
-        une liste à jour des alumni du Collège et de l’Ecole de droit et
-        d’établir des statistiques sur l’insertion professionnelle des diplômés
-        du Collège et de l’Ecole de droit de l’Université Paris Panthéon-Assas.
-        La base légale du traitement est l’intérêt légitime de l’Association du
-        Collège et de l’Ecole de droit.
+        L’Association du Collège et de l’Ecole de droit se réserve la
+        possibilité d’utiliser les informations disponibles sur le site de
+        l’annuaire, aux fins de tenir une liste à jour des alumni du Collège et
+        de l’Ecole de droit et d’établir des statistiques sur l’insertion
+        professionnelle des diplômés du Collège et de l’Ecole de droit de
+        l’Université Paris Panthéon-Assas. La base légale du traitement est
+        l’intérêt légitime de l’Association du Collège et de l’Ecole de droit.
       </p>
 
       <p className="w-[90%] text-gray-400 text-xs mx-auto my-4">
@@ -40,17 +42,23 @@ function RGPD() {
         Vous pouvez accéder aux données vous concernant, les rectifier, demander
         leur effacement ou exercer votre droit à la limitation du traitement de
         vos données. Vous pouvez également vous opposer au traitement de vos
-        données. Consultez le site www.cnil.fr pour plus d’informations sur vos
-        droits.
+        données. Consultez le site{" "}
+        <a href="http://www.cnil.fr" target="_blank">
+          www.cnil.fr
+        </a>{" "}
+        pour plus d’informations sur vos droits.
       </p>
 
       <p className="w-[90%] text-gray-400 text-xs mx-auto my-4">
         Pour exercer ces droits ou pour toute question sur le traitement de vos
         données dans ce dispositif, vous pouvez contacter le secrétaire général
-        de l’Association ou son adjoint à l’adresse cdd.edd.paris2@gmail.com. Si
-        vous estimez, après nous avoir contactés, que vos droits « Informatique
-        et Libertés » ne sont pas respectés, vous pouvez adresser une
-        réclamation à la CNIL. »
+        de l’Association ou son adjoint à l’adresse{" "}
+        <a href="mailto:cdd.edd.paris2@gmail.com" target="_blank">
+          cdd.edd.paris2@gmail.com
+        </a>
+        . Si vous estimez, après nous avoir contactés, que vos droits «
+        Informatique et Libertés » ne sont pas respectés, vous pouvez adresser
+        une réclamation à la CNIL.
       </p>
       <Footer />
     </div>
