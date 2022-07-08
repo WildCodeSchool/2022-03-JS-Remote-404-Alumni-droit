8eca92c7580af671873d327260c60e7142ccad81
ismamamama1 <ismael.agb@hotmail.fr>
2022-07-08 11:23:04 +0200

    Deleted MasterFilter component to apply hook form on elements on the same page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 import years from "@assets/years";
-import MasterFilter from "@components/MasterFilter";
 import { TextField, Autocomplete } from "@mui/material";
 // import { styled } from "@mui/material/styles";
 import Switch from "@mui/material/Switch";
@@ -22,6 +21,7 @@ function SignUp() {
   console.warn(errors);
 
   const [diplomeInput, setDiplomeInput] = useState([true, false, false]);
+  const [masterInput, setMasterInput] = useState([true, false, false]);
 
   const handleDiplomeInput = (index, type) => {
     const provDiplomeInput = [...diplomeInput];
@@ -33,8 +33,19 @@ function SignUp() {
     setDiplomeInput(provDiplomeInput);
   };
 
+  const handleMasterInput = (index, type) => {
+    const provMasterInput = [...masterInput];
+    if (index < 2 && type === "plus") {
+      provMasterInput[index + 1] = true;
+    } else if (index > 0 && type === "minus") {
+      provMasterInput[index] = false;
+    }
+    setMasterInput(provMasterInput);
+  };
+
   const [diplomeData, setDiplomeData] = useState([]);
   const [professionData, setProfessionData] = useState([]);
+  const [masterData, setMasterData] = useState([]);
 
   const getDiplome = () => {
     axios
@@ -58,9 +69,20 @@ function SignUp() {
       });
   };
 
+  const getMaster = () => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/master`)
+      .then((res) => {
+        setMasterData(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   useEffect(() => {
     getDiplome();
     getProfession();
+    getMaster();
   }, []);
 
   // const AntSwitch = styled(Switch)(({ theme }) => ({
@@ -279,7 +301,86 @@ function SignUp() {
                 Champs optionnels
                 {/* BLOC MASTER ANNEE */}
               </h3>
-              <MasterFilter />
+              <div className="flex flex-col lg:border lg:p-4 rounded">
+                <p className="lg:text-sm text-center mb-6 ">
+                  Votre cursus annexe :
+                </p>
+                {masterInput.map((masterIn, index) => {
+                  if (masterIn) {
+                    return (
+                      <div
+                        key={Date.now()}
+                        className="flex flex-wrap justify-between mb-5"
+                      >
+                        <Autocomplete
+                          disablePortal
+                          id="combo-box-4"
+                          options={masterData}
+                          getOptionLabel={(option) =>
+                            option.title.replace("&apos;E", "'É")
+                          }
+                          sx={{
+                            width: "55%",
+                            mb: 1.5,
+                          }}
+                          renderInput={(params) => (
+                            <TextField
+                              {...params}
+                              label={`#${index + 1} Master 2`}
+                              color="primary"
+                            />
+                          )}
+                        />
+                        <TextField
+                          label="Année"
+                          color="primary"
+                          sx={{
+                            width: "30%",
+                          }}
+                        />
+                        <button
+                          type="button"
+                          disabled={index === 2}
+                          className="pb-3"
+                          onClick={() => handleMasterInput(index, "plus")}
+                        >
+                          <HiPlus
+                            size={20}
+                            color={index === 2 ? "#d3d3d3" : "black"}
+                          />
+                        </button>
+                        <button
+                          type="button"
+                          disabled={index === 0}
+                          className="pb-3"
+                          onClick={() => handleMasterInput(index, "minus")}
+                        >
+                          <HiMinus
+                            size={20}
+                            color={index === 0 ? "#d3d3d3" : "black"}
+                          />
+                        </button>
+                        <TextField
+                          label="Entrer un master"
+                          size="medium"
+                          sx={{
+                            width: "55%",
+                            mb: 1.5,
+                          }}
+                        />
+                        <TextField
+                          label="Université d'obtention"
+                          size="medium"
+                          sx={{
+                            width: "100%",
+                          }}
+                        />
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
               <TextField label="Employeur actuel" size="medium" />
               <TextField
                 label="Email professionnel"
